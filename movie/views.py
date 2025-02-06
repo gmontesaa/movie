98636edaed318ae7a9f3c3b0ba0fe1a7420a6aa9
gmontesaa <gmontesa@eafit.edu.co>
2025-02-06 12:41:34 -0500

    busqueda de peliculas

--- movie/views.py
+++ movie/views.py
@@ -9,8 +9,11 @@ def home(request):
     #return render(request,'home.html')
     #return render(request, 'home.html',{'name':'Geronimo Montes'})
     searchTerm = request.GET.get('searchMovie')
-    movies = Movie.objects.all()
-    return render(request, 'home.html',{'searchTerm':searchTerm,'movies':movies})
+    if searchTerm:
+        movies = Movie.objects.filter(title__icontains=searchTerm)
+    else:
+        movies = Movie.objects.all()
+    return render(request, 'home.html', {'searchTerm': searchTerm, 'movies': movies})
 
 def about(request):
     #return HttpResponse("<h1>Welcome to about page</h1>")
